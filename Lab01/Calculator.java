1861391162c9d7fa068c16167edc3f429b48296f
Nhat Minh Vu <nhatminh140303@gmail.com>
2023-03-31 18:09:22 +0700

    2.2.5 and 2.26

--- Lab01/Calculator.java
+++ Lab01/Calculator.java
@@ -1,36 +1,32 @@
-// Write a program to calculate sum, difference, product, and quotient of 2 double numbers which 
-// are entered by users.
+// 2.2.5 Write a program to calculate sum, difference, product, and quotient of 2 double numbers which are entered by users.
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Problem225 {
+public class Calculator {
     public static void main(String[] args) {
         // Prompt the user input
         Scanner myScanner = new Scanner(System.in);
 
-        double num1 = 0.0;
-        double num2 = 0.0;
+        double num1 = 0;
+        double num2 = 0;
 
-        // Handling error
+        // Throw an exception if input is not valid
         try {
             System.out.print("Enter first number: ");
             num1 = myScanner.nextDouble();  
-
             System.out.print("Enter second number: ");
             num2 = myScanner.nextDouble();           
         }
         catch (InputMismatchException exception) {
-            System.out.println("Invalid input");
+            System.out.println("Input must be a double");
             System.exit(0);
         }
 
         if (num2 == 0) {
-            System.out.println("Zero division error");
+            System.out.println("The divisor must be a nonzero");
             System.exit(0);
         }
-
-        myScanner.close();
 
         // Calculate result
         double sum = num1 + num2;
@@ -38,10 +34,12 @@ public class Problem225 {
         double product = num1 * num2;
         double quotient = num1 / num2;
 
-        // Output result
+        // Print result
         System.out.println("Sum: " + sum);
         System.out.println("Difference: " + difference);
         System.out.println("Product: " + product);
         System.out.println("Quotient: " + quotient);
+
+        myScanner.close();
     }
 }
