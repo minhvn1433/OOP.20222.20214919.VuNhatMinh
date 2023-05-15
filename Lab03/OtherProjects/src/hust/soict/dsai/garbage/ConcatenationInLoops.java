884b4d29ca32d00848e3d706661406a28ffc89b1
Nhat Minh Vu <nhatminh140303@gmail.com>
2023-05-15 10:30:28 +0700

    Initial commit

--- Lab03/OtherProjects/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
+++ Lab03/OtherProjects/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
@@ -1,4 +1,5 @@
 package hust.soict.dsai.garbage;
+import java.util.Random;
 
 public class ConcatenationInLoops {
     public static void main(String[] args) {
@@ -7,7 +8,7 @@ public class ConcatenationInLoops {
         String s = "";
         for (int i = 0; i < 65536; i++)
             s += r.nextInt(2);
-        System.out.println(System.currentTimeMillis() - start); // This prints roughly 4500.
+        System.out.println(System.currentTimeMillis() - start);
 
         r = new Random(123);
         start = System.currentTimeMillis();
@@ -15,6 +16,6 @@ public class ConcatenationInLoops {
         for (int i = 0; i < 65536; i++)
             sb.append(r.nextInt(2));
         s = sb.toString();
-        System.out.println(System.currentTimeMillis() - start); // This print S.
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
